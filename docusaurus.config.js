08905f07c8fc47ebfc8447f0ea0bbd593fd8f6f4
Moon <moonispsace@gmail.com>
2022-03-27 19:13:28 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ const config = {
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/moonisspace',
               },
             ],
           },
