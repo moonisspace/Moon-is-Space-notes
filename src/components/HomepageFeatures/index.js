7c346db729f3f1836aa13a04e521ae4b46adcdc0
Moon <moonispsace@gmail.com>
2022-03-27 19:26:16 +0100

    update

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -8,8 +8,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Introduction shows general timeline of the project. It also shows personal reflections and thoughts
       </>
     ),
   },
