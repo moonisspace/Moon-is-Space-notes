3cf6b853b2f29963dba244d7765b480fab28bc3e
Moon <moonispsace@gmail.com>
2022-03-27 19:19:42 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
         title: 'Moon is Space',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/moonisspace.png',
         },
         items: [
           {
